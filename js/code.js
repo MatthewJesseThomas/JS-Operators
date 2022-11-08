8aed4a8168f92ef9075d8eb89012432927adb65b
MatthewJesseThomas <owlsaintgermain@gmail.com>
2022-11-08 13:39:20 +0200

    main

---
+++ js/code.js
@@ -0,0 +1,43 @@
+//Arithmetic Operators
+//Addition
+function addition() {
+    console.log(2+2);
+}
+//Subtraction
+function subtraction() {
+    console.log(5-2);
+}
+//Multiplication
+function multiplication() {
+    console.log(2*2);
+}
+//Division
+function division(){
+    console.log(5/2);
+}
+
+function exponent() {
+    console.log(2**2);
+}
+//Modulus
+function modulus() {
+    console.log(5%2);
+}
+//Call all Functions
+// addition();
+// subtraction();
+// multiplication();
+// division();
+// exponent();
+// modulus();
+//Assignment Operators
+//Addition Equals
+// let numb1 = 4;
+// let numb2 = 6;
+// let result = 0;
+// result = numb1 + numb2;
+// console.log(result);
+// let numb1 = 3;
+// numb1 = numb1 + 2;
+// numb1 -= 2 ;
+// console.log(numb1)
